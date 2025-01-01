612e53193389c252ffd58eb7e8f1434e7bea5066
yuyu <yuyunjae03@gmail.com>
2025-01-01 18:01:41 +0900

    maybe cpp00_ex01 complete.. init cpp00_ex02

--- cpp_00/ex01/Contact.hpp
+++ cpp_00/ex01/Contact.hpp
@@ -1,35 +1,38 @@
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 class Contact {
-   private:
-    std::string firstName;
-    std::string lastName;
-    std::string nickname;
-    std::string phoneNumber;
-    std::string darkestSecret;
-    bool checkIsPrint(const std::string &str);
-    bool checkInput(const std::string &str);
-    bool checkCin();
+ private:
+  std::string firstName;
+  std::string lastName;
+  std::string nickname;
+  std::string phoneNumber;
+  std::string darkestSecret;
 
-   public:
-    Contact();
-    Contact(std::string firstName, std::string lastName, std::string nickname,
-            std::string phoneNumber, std::string darkestSecret);
-    ~Contact();
-    Contact &operator=(const Contact &other);
-    bool setContact();
-    bool setContact(std::string firstName, std::string lastName,
-                    std::string nickname, std::string phoneNumber,
-                    std::string darkestSecret);
-    const std::string &getFirstName();
-    const std::string &getLastName();
-    const std::string &getNickname();
-    const std::string &getPhoneNumber();
-    const std::string &getDarkestSecret();
+  bool checkIsPrint(const std::string &str);
+  bool checkInput(const std::string &str);
+  //   bool checkCin();
+
+ public:
+  Contact();
+  Contact(std::string firstName, std::string lastName, std::string nickname,
+          std::string phoneNumber, std::string darkestSecret);
+  ~Contact();
+  Contact &operator=(const Contact &other);
+
+  bool setContact();
+  bool setContact(std::string firstName, std::string lastName,
+                  std::string nickname, std::string phoneNumber,
+                  std::string darkestSecret);
+  const std::string &getFirstName();
+  const std::string &getLastName();
+  const std::string &getNickname();
+  const std::string &getPhoneNumber();
+  const std::string &getDarkestSecret();
 };
 
 #endif
--- cpp_00/ex01/PhoneBook.hpp
+++ cpp_00/ex01/PhoneBook.hpp
@@ -1,22 +1,28 @@
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
-#include "Contact.hpp"
 #include <iostream>
 #include <string>
 
-class PhoneBook {
-   private:
-    int top;
-    int phoneBookSize;
-    Contact contacts[8];
-    void addContact();
-    void searchContact();
+#include "Contact.hpp"
 
-   public:
-    PhoneBook();
-    ~PhoneBook();
-    static bool program(PhoneBook &phoneBook);
+class PhoneBook {
+ private:
+  int top;
+  int phoneBookSize;
+  Contact contacts[8];
+
+  void addContact();
+  void searchContact();
+  void printSimpleForm(const std::string &str);
+  void printSimple(int contactIndex, int index);
+  void printContactAll(int contactIndex);
+
+ public:
+  PhoneBook();
+  ~PhoneBook();
+
+  static bool program(PhoneBook &phoneBook);
 };
 
 #endif
