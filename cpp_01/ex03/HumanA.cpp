4f7cc311d8ae4d3ff14585faa7567db54fd0792e
yuyu <yuyunjae03@gmail.com>
2025-01-04 21:11:38 +0900

    finish cpp_01 ex03.. will do ex04

---
+++ cpp_01/ex03/HumanA.cpp
@@ -0,0 +1,13 @@
+#include "HumanA.hpp"
+
+HumanA::HumanA(const std::string& name, Weapon& weapon)
+    : name(name), weapon(weapon) {}
+
+HumanA::~HumanA() {}
+
+void HumanA::setWeapon(Weapon& weapon) { this->weapon = weapon; }
+
+void HumanA::attack() {
+  std::cout << this->name << " attacks with their " << this->weapon.getType()
+            << std::endl;
+}
