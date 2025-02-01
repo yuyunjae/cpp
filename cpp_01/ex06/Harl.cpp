95fb2acb2085b8e685734b28c14d4b18d6a6aefc
yuyu <yuyunjae03@gmail.com>
2025-02-01 16:53:40 +0900

    make cpp01/ex06 switch case statement

--- cpp_01/ex06/Harl.cpp
+++ cpp_01/ex06/Harl.cpp
@@ -3,19 +3,18 @@
 const std::string Harl::str[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 Harl::Harl() {
-    this->debugMsg =
-        "I love having extra bacon for my "
-        "7XL-double-cheese-triple-pickle-specialketchup burger.\nI "
-        "really do!";
-    this->infoMsg =
-        "I cannot believe adding extra bacon costs more money.\nYou "
-        "didn't put enough bacon in my burger!\nIf you did, I wouldn't "
-        "be asking for more!";
-    this->warningMsg =
-        "I think I deserve to have some extra bacon for free.\nI've been "
-        "coming for years whereas you started working here since last month.";
-    this->errorMsg =
-        "This is unacceptable, I want to speak to the manager now.";
+  this->debugMsg =
+      "I love having extra bacon for my "
+      "7XL-double-cheese-triple-pickle-specialketchup burger.\nI "
+      "really do!";
+  this->infoMsg =
+      "I cannot believe adding extra bacon costs more money.\nYou "
+      "didn't put enough bacon in my burger!\nIf you did, I wouldn't "
+      "be asking for more!";
+  this->warningMsg =
+      "I think I deserve to have some extra bacon for free.\nI've been "
+      "coming for years whereas you started working here since last month.";
+  this->errorMsg = "This is unacceptable, I want to speak to the manager now.";
 }
 
 Harl::Harl(std::string debugMsg, std::string infoMsg, std::string warningMsg,
@@ -23,43 +22,53 @@ Harl::Harl(std::string debugMsg, std::string infoMsg, std::string warningMsg,
     : debugMsg(debugMsg),
       infoMsg(infoMsg),
       warningMsg(warningMsg),
-      errorMsg(errorMsg) {
-}
+      errorMsg(errorMsg) {}
 
-Harl::~Harl() {
-}
+Harl::~Harl() {}
 
 void Harl::debug(void) {
-    std::cout << "[ DEBUG ]\n";
-    std::cout << this->debugMsg << "\n\n";
+  std::cout << "[ DEBUG ]\n";
+  std::cout << this->debugMsg << "\n\n";
 }
 
 void Harl::info(void) {
-    std::cout << "[ INFO ]\n";
-    std::cout << this->infoMsg << "\n\n";
+  std::cout << "[ INFO ]\n";
+  std::cout << this->infoMsg << "\n\n";
 }
 void Harl::warning(void) {
-    std::cout << "[ WARNING ]\n";
-    std::cout << this->warningMsg << "\n\n";
+  std::cout << "[ WARNING ]\n";
+  std::cout << this->warningMsg << "\n\n";
 }
 void Harl::error(void) {
-    std::cout << "[ ERROR ]\n";
-    std::cout << this->errorMsg << "\n\n";
-    // 이부분 개행 하나 뺼지말지 고민해보기..
+  std::cout << "[ ERROR ]\n";
+  std::cout << this->errorMsg << "\n\n";
+  // 이부분 개행 하나 뺼지말지 고민해보기..
 }
 
 void Harl::complain(std::string level) {
-    void (Harl::* callFunction[4])() = {};
-    callFunction[0] = &Harl::debug;
-    callFunction[1] = &Harl::info;
-    callFunction[2] = &Harl::warning;
-    callFunction[3] = &Harl::error;
+  void (Harl::*callFunction[4])() = {};
+  callFunction[0] = &Harl::debug;
+  callFunction[1] = &Harl::info;
+  callFunction[2] = &Harl::warning;
+  callFunction[3] = &Harl::error;
 
-    for (int i = 0; i < 4; i++) {
-        if (!str[i].compare(level)) {
-            for (int j = i; j < 4; j++) (this->*callFunction[j])();
-            return;
-        }
+  int i;
+  for (i = 0; i < 4; i++) {
+    if (!str[i].compare(level)) {
+      break;
     }
-    std::cout << "[ Probably complaining about insignificant problems ]\n";
+  }
+  switch (i) {
+    case 0:
+      (this->*callFunction[0])();
+    case 1:
+      (this->*callFunction[1])();
+    case 2:
+      (this->*callFunction[2])();
+    case 3:
+      (this->*callFunction[3])();
+      break;
+    default:
+      std::cout << "[ Probably complaining about insignificant problems ]\n";
+  }
 }
