81f42ad27d4f07c46f817198809d61e998cd7cdd
yuyu <yuyunjae03@gmail.com>
2025-02-18 21:20:41 +0900

    make cpp-5

--- cpp_05/ex03/main.cpp
+++ cpp_05/ex03/main.cpp
@@ -1,3 +1,5 @@
+// #include <stdio.h>
+
 #include "AForm.hpp"
 #include "Bureaucrat.hpp"
 #include "Intern.hpp"
@@ -5,13 +7,16 @@
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
+void leak(void) { system("leaks Bureaucrat"); }
+
 int main(void) {
+  // atexit(leak);
   std::cout << "-------------------- test1 : ShrubberyCreationForm 정상 생성"
                "--------------------\n";
+  AForm *rrf;
   try {
     Bureaucrat test1("yuyu", 3);
     Intern intern1;
-    AForm *rrf;
     rrf = intern1.makeForm("shrubbery creation", "Bender1");
     std::cout << *rrf << std::endl;
     test1.signForm(*rrf);
@@ -19,13 +24,14 @@ int main(void) {
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+  delete rrf;
+  rrf = 0;
 
   std::cout << "-------------------- test2 : PresidentialPardonForm 정상 생성"
                "--------------------\n";
   try {
     Bureaucrat test1("yuyu", 3);
     Intern intern1;
-    AForm *rrf;
     rrf = intern1.makeForm("presidential pardon", "Bender2");
     std::cout << *rrf << std::endl;
     test1.signForm(*rrf);
@@ -33,13 +39,14 @@ int main(void) {
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+  delete rrf;
+  rrf = 0;
 
   std::cout << "-------------------- test3 : RobotomyRequestForm 정상 생성"
                "--------------------\n";
   try {
     Bureaucrat test1("yuyu", 3);
     Intern intern1;
-    AForm *rrf;
     rrf = intern1.makeForm("robotomy request", "Bender3");
     std::cout << *rrf << std::endl;
     test1.signForm(*rrf);
@@ -47,13 +54,14 @@ int main(void) {
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+  delete rrf;
+  rrf = 0;
 
   std::cout << "-------------------- test4 : 존재하지않는 Form 생성 시도"
                "--------------------\n";
   try {
     Bureaucrat test1("yuyu", 3);
     Intern intern1;
-    AForm *rrf;
     rrf = intern1.makeForm("Non-existent form", "Bender4");
     std::cout << *rrf << std::endl;
     test1.signForm(*rrf);
@@ -61,4 +69,6 @@ int main(void) {
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+  delete rrf;
+  rrf = 0;
 }
