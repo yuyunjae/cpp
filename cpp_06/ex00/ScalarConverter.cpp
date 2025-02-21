4d6dad86e522dc0c3d83623f09d2e174f68e6b2e
yuyu <yuyunjae03@gmail.com>
2025-02-21 21:55:47 +0900

    fix cpp_06/ex00

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -37,12 +37,6 @@ void ScalarConverter::printNan() {
 
 void ScalarConverter::printLiterals(char ch, int ivalue, float fvalue,
                                     double dvalue, int flag) {
-  if (flag & (1 << 11) || flag & 1) {
-    std::cout
-        << "char: impossible\nint: impossible\nfloat: nanf\ndouble: nan\n";
-    return;
-  }
-
   std::cout << "char: ";
   if (flag & (1 << 3) || flag & (1 << 1))
     std::cout << "overflow\n";
@@ -62,20 +56,10 @@ void ScalarConverter::printLiterals(char ch, int ivalue, float fvalue,
     std::cout << ivalue << std::endl;
 
   std::cout << "float: ";
-  if (flag & (1 << 7))
-    std::cout << "overflow\n";
-  else if (flag & (1 << 8))
-    std::cout << "underflow\n";
-  else
-    std::cout << fvalue << 'f' << std::endl;
+  std::cout << fvalue << ((fvalue - ivalue) ? "f\n" : ".0f\n");
 
   std::cout << "double: ";
-  if (flag & (1 << 9))
-    std::cout << "overflow\n";
-  else if (flag & (1 << 10))
-    std::cout << "underflow\n";
-  else
-    std::cout << dvalue << std::endl;
+  std::cout << dvalue << ((fvalue - ivalue) ? "\n" : ".0\n");
 }
 
 int ScalarConverter::checkType(const std::string &str) {
@@ -93,6 +77,9 @@ int ScalarConverter::checkType(const std::string &str) {
   else if (str == "-inf" || str == "+inf" || str == "inf")
     return DOUBLETYPE;
 
+  // 문자
+  if (str.length() == 1 && !std::isdigit(str[0])) return CHARTYPE;
+
   // 숫자만으로 이루어져있는지 체크
   for (std::string::size_type i = 0; i < str.length(); i++) {
     if (!std::isdigit(str[i])) {
@@ -103,9 +90,7 @@ int ScalarConverter::checkType(const std::string &str) {
     }
   }
 
-  if (str.length() == 1 && !std::isdigit(str[0]))
-    return CHARTYPE;
-  else if (dot == std::string::npos && f == std::string::npos)
+  if (dot == std::string::npos && f == std::string::npos)
     return INTTYPE;
   else if (dot != std::string::npos && f == std::string::npos)
     return DOUBLETYPE;
@@ -127,69 +112,154 @@ void ScalarConverter::convertChar(const std::string &str) {
   printLiterals(ch, ivalue, fvalue, dvalue, 0);
 }
 
+void ScalarConverter::convertInt(const std::string &str) {
+  char ch = 0;
+  int ivalue = 0;
+  float fvalue = 0;
+  double dvalue = 0;
+  char flag = 0;
+  /*
+  1      nan
+  1 << 1 Inf
+  1 << 2 -Inf
+  1 << 3 char overflow
+  1 << 4 char underflow
+  1 << 5 int overflow
+  1 << 6 int underflow
+  */
+  char *endptr = 0;
+  std::stringstream ss;
+  std::string check;
+  errno = 0;
+
+  ivalue = atoi(str.c_str());
+  ss << ivalue;
+  ss >> check;
+  if (str == check || (str[0] == '+' && str.substr(1) == check) ||
+      (ivalue == 0 && str[0] == '-' && str[1] == '0')) {  // common case
+    if (ivalue > 127)
+      flag |= 1 << 3;
+    else if (ivalue < -128)
+      flag |= 1 << 4;
+    fvalue = static_cast<float>(ivalue);
+    dvalue = static_cast<double>(ivalue);
+  } else {
+    if (str[0] == '-') {  // underflow
+      flag |= 1 << 4;
+      flag |= 1 << 6;
+    } else {  // overflow
+      flag |= 1 << 3;
+      flag |= 1 << 5;
+    }
+    dvalue = std::strtod(str.c_str(), &endptr);
+    if (*endptr != '\0' && *endptr != 'f') return printNan();
+    fvalue = static_cast<float>(dvalue);
+  }
+  ch = static_cast<char>(ivalue);
+  printLiterals(ch, ivalue, fvalue, dvalue, flag);
+}
+
+void ScalarConverter::convertFloat(const std::string &str) {
+  char ch = 0;
+  int ivalue = 0;
+  float fvalue = 0;
+  double dvalue = 0;
+  char flag = 0;
+  char *endptr = 0;
+
+  // inf 처리
+  if (str == "+inff" || str == "inff") {
+    fvalue = std::numeric_limits<float>::infinity();
+    dvalue = std::numeric_limits<double>::infinity();
+    flag |= 1 << 1;
+    return printLiterals(ch, ivalue, fvalue, dvalue, flag);
+  } else if (str == "-inff") {
+    fvalue = -std::numeric_limits<float>::infinity();
+    dvalue = -std::numeric_limits<double>::infinity();
+    flag |= 1 << 2;
+    return printLiterals(ch, ivalue, fvalue, dvalue, flag);
+  }
+  errno = 0;
+  fvalue = std::strtof(str.c_str(), &endptr);
+  if (*endptr != '\0' && *endptr != 'f') return printNan();
+
+  if (errno == ERANGE) {  // overflow occur
+    if (str[0] == '-') {
+      flag |= 1 << 4;
+      flag |= 1 << 6;
+    } else {
+      flag |= 1 << 3;
+      flag |= 1 << 5;
+    }
+    dvalue = std::strtod(str.c_str(), &endptr);
+    if (*endptr != '\0' && *endptr != 'f') return printNan();
+  } else {  // common case
+    if (fvalue > CHAR_MAX) {
+      flag |= 1 << 3;
+      if (fvalue > INT_MAX) flag |= 1 << 5;
+    } else if (fvalue < CHAR_MIN) {
+      flag |= 1 << 4;
+      if (fvalue < INT_MIN) flag |= 1 << 6;
+    }
+    dvalue = static_cast<double>(fvalue);
+  }
+  ch = static_cast<char>(fvalue);
+  ivalue = static_cast<int>(fvalue);
+
+  printLiterals(ch, ivalue, fvalue, dvalue, flag);
+}
+
 void ScalarConverter::convertDouble(const std::string &str) {
   char ch = 0;
   int ivalue = 0;
   float fvalue = 0;
   double dvalue = 0;
+  char flag = 0;
   char *endptr = 0;
 
   if (str == "+inf" || str == "inf") {
     fvalue = std::numeric_limits<float>::infinity();
     dvalue = std::numeric_limits<double>::infinity();
+    flag |= 1 << 1;
+    return printLiterals(ch, ivalue, fvalue, dvalue, flag);
   } else if (str == "-inf") {
     fvalue = -std::numeric_limits<float>::infinity();
     dvalue = -std::numeric_limits<double>::infinity();
+    flag |= 1 << 2;
+    return printLiterals(ch, ivalue, fvalue, dvalue, flag);
   }
+
   errno = 0;
   dvalue = std::strtod(str.c_str(), &endptr);
   if (*endptr != '\0' && *endptr != 'f') return printNan();
 
   if (errno == ERANGE) {
     if (str[0] == '-') {
-      flag += 1 << 4;
-      flag += 1 << 6;
-      flag += 1 << 8;
-      flag += 1 << 10;
+      flag |= 1 << 4;
+      flag |= 1 << 6;
     } else {
-      flag += 1 << 3;
-      flag += 1 << 5;
-      flag += 1 << 7;
-      flag += 1 << 9;
+      flag |= 1 << 3;
+      flag |= 1 << 5;
     }
   } else {
     if (dvalue > CHAR_MAX) {
-      flag += 1 << 3;
-      if (dvalue > INT_MAX) {
-        flag += 1 << 5;
-        if (dvalue > std::numeric_limits<float>::max())
-          flag += 1 << 7;
-        else
-          fvalue = static_cast<float>(dvalue);
-      } else
-        ivalue = static_cast<int>(dvalue);
+      flag |= 1 << 3;
+      if (dvalue > INT_MAX) flag |= 1 << 5;
     } else if (dvalue < CHAR_MIN) {
-      flag += 1 << 4;
-      if (dvalue < INT_MIN) {
-        flag += 1 << 6;
-        if (dvalue < std::numeric_limits<float>::min())
-          flag += 1 << 8;
-        else
-          fvalue = static_cast<float>(dvalue);
-      } else
-        ivalue = static_cast<int>(dvalue);
-    } else {
-      ch = static_cast<char>(dvalue);
-      ivalue = static_cast<int>(dvalue);
-      fvalue = static_cast<float>(dvalue);
+      flag |= 1 << 4;
+      if (dvalue < INT_MIN) flag |= 1 << 6;
     }
   }
+  ch = static_cast<char>(dvalue);
+  ivalue = static_cast<int>(dvalue);
+  fvalue = static_cast<float>(dvalue);
+  printLiterals(ch, ivalue, fvalue, dvalue, flag);
 }
 
 void ScalarConverter::convert(const std::string str) {
   std::string substr = trim(str);
 
-  // 빈공간만 있을 때
+  // trim이후 어떤 문자도 없음.
   if (substr.length() < 1) return printNan();
   int literalType = checkType(substr);
   switch (literalType) {
@@ -211,163 +281,4 @@ void ScalarConverter::convert(const std::string str) {
     default:
       printNan();
   }
-
-  std::string::size_type dot;
-  std::string::size_type f;
-
-  int flag = 0;
-  /*
-  1      nan
-  1 << 1 Inf
-  1 << 2 -Inf
-  1 << 3 char overflow
-  1 << 4 char underflow
-  1 << 5 int overflow
-  1 << 6 int underflow
-  1 << 7 float overflow
-  1 << 8 float underflow
-  1 << 9 double overflow
-  1 << 10 double underflow
-  1 << 11 Not a value error
-  1 << 12 char's Non displayable // 이건 printLiterals에서 체크하는게 맞을 듯
-  */
-
-  if (substr.length() < 1) {  // 빈공간만 있을 때
-    if (str.length() == 1 && str[0] == ' ')
-      substr = " ";
-    else {
-      flag += 1 << 11;
-      return printLiterals(ch, ivalue, fvalue, dvalue, flag);
-    }
-  }
-  dot = substr.find('.');
-  f = substr.find('f', substr.length() - 1);
-
-  // char
-  if (substr.length() == 1 && !std::isdigit(substr[0])) {
-    ch = static_cast<char>(substr[0]);
-    ivalue = static_cast<int>(ch);
-    fvalue = static_cast<float>(ch);
-    dvalue = static_cast<double>(ch);
-  }
-
-  // int
-  else if (dot == std::string::npos && f == std::string::npos) {
-    std::stringstream ss;
-    std::string check;
-    ivalue = atoi(substr.c_str());
-    ss << ivalue;
-    ss >> check;
-    if (substr == check || (substr[0] == '+' && substr.substr(1) == check)) {
-      if (ivalue >= -128 && ivalue <= 127)
-        ch = static_cast<char>(ivalue);
-      else if (ivalue > 0)
-        flag += 1 << 3;
-      else
-        flag += 1 << 4;
-    } else if (substr[0] == '-') {
-      flag += 1 << 4;
-      flag += 1 << 6;
-      // float double underflow 여부 체크해야함.
-    } else {  // overflow
-      flag += 1 << 3;
-      flag += 1 << 5;
-    }
-    fvalue = static_cast<float>(ivalue);
-    dvalue = static_cast<double>(ivalue);
-  }
-
-  // float
-  else if (dot != std::string::npos && f != std::string::npos) {
-    errno = 0;
-    char *endptr = 0;
-
-    fvalue = std::strtof(substr.c_str(), &endptr);
-    if (*endptr != '\0' && *endptr != 'f') {
-      flag += 1 << 11;
-      return printLiterals(ch, ivalue, fvalue, dvalue, flag);
-    }
-    if (errno == ERANGE) {
-      if (substr[0] == '-') {
-        flag += 1 << 4;
-        flag += 1 << 6;
-        flag += 1 << 8;
-      } else {
-        flag += 1 << 3;
-        flag += 1 << 5;
-        flag += 1 << 7;
-      }
-    } else {
-      if (fvalue > CHAR_MAX) {
-        flag += 1 << 3;
-        if (fvalue > INT_MAX)
-          flag += 1 << 5;
-        else
-          ivalue = static_cast<int>(fvalue);
-      } else if (fvalue < CHAR_MIN) {
-        flag += 1 << 4;
-        if (fvalue < INT_MIN)
-          flag += 1 << 6;
-        else
-          ivalue = static_cast<int>(fvalue);
-      } else {
-        ch = static_cast<char>(fvalue);
-        ivalue = static_cast<int>(fvalue);
-      }
-    }
-    dvalue = static_cast<float>(fvalue);
-  }
-
-  // double
-  else if (dot != std::string::npos && f == std::string::npos) {
-    errno = 0;
-    char *endptr = 0;
-
-    dvalue = std::strtod(substr.c_str(), &endptr);
-    if (*endptr != '\0' && *endptr != 'f') {
-      flag += 1 << 11;
-      return printLiterals(ch, ivalue, fvalue, dvalue, flag);
-    }
-
-    if (errno == ERANGE) {
-      if (substr[0] == '-') {
-        flag += 1 << 4;
-        flag += 1 << 6;
-        flag += 1 << 8;
-        flag += 1 << 10;
-      } else {
-        flag += 1 << 3;
-        flag += 1 << 5;
-        flag += 1 << 7;
-        flag += 1 << 9;
-      }
-    } else {
-      if (dvalue > CHAR_MAX) {
-        flag += 1 << 3;
-        if (dvalue > INT_MAX) {
-          flag += 1 << 5;
-          if (dvalue > std::numeric_limits<float>::max())
-            flag += 1 << 7;
-          else
-            fvalue = static_cast<float>(dvalue);
-        } else
-          ivalue = static_cast<int>(dvalue);
-      } else if (dvalue < CHAR_MIN) {
-        flag += 1 << 4;
-        if (dvalue < INT_MIN) {
-          flag += 1 << 6;
-          if (dvalue < std::numeric_limits<float>::min())
-            flag += 1 << 8;
-          else
-            fvalue = static_cast<float>(dvalue);
-        } else
-          ivalue = static_cast<int>(dvalue);
-      } else {
-        ch = static_cast<char>(dvalue);
-        ivalue = static_cast<int>(dvalue);
-        fvalue = static_cast<float>(dvalue);
-      }
-    }
-  }
-  return printLiterals(ch, ivalue, fvalue, dvalue, flag);
 }
